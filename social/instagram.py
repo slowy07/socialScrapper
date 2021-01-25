7cc3d890cb5fafa6ce315c0fcd3b00d36f0b4d35
slowy07 <slowy.arfy@gmail.com>
2021-01-25 14:12:19 +0700

    instagram

--- social/instagram.py
+++ social/instagram.py
@@ -56,3 +56,69 @@ def Instagram(user):
                         f1.write("")
                     else:
                         f1.write("\nCaption:")
+                    f1.write("\nShortCode :"+i["node"]["shortcode"])
+                    f1.write("\nComment :"+str(i[node]["edge_media_to_comment"]['count']))
+                    media1 = igql_api.get_media(i['node']["shortcode"])
+                    comments = []
+                    for comments_batch in media1.comments():
+                        comments.append(comments_batch)
+                    for comments_batch in comments[0]:
+                        j1 = ast.literal_eval(str(comments_batch['node']))
+                        f1.write("\nOwner :"+j1['owner']['username'])
+                        f1.write("\nText :"+j1['text'])
+                        if j1['text'] in wordlist:
+                            predator.append(j1['owner']['username'])
+                            print(R+"{} may be a predator".format(j1['owner']['username'])+W)
+                    
+                    f1.write("\nTimestamp:"+ str(i['node']["taken_at_timestamp"]))
+                    f1.write("\nDimensions:"+str(i['node']["dimensions"]['height'])+" X "+str(i['node']["dimensions"]['width']))
+                    f1.write("\nURL:"+str(i['node']["display_url"]))
+                    urllib.request.urlretrieve(str(i['node']["display_url"]),str(username1)+"/"+i['node']["id"]+".jpg")
+                    f1.write("\nLikes:"+str(i['node']["edge_liked_by"]['count']))
+                    f1.write("\nLocation:"+str(i['node']["location"]))
+                    f1.write("\nOwner:"+str(i['node']["owner"]["username"]))
+                    f1.write("accessibility_caption:"+str(i['node']["accessibility_caption"]))
+                    if str(i['node']["accessibility_caption"]) in wordlist:
+                        f1.write(R+"Image may Contain Insecure Content")
+            '''p=machine(username1)
+            if p==True:
+                predator.append(j1['owner']['username'])
+            else:
+                pass'''
+            f1.close()
+            print(W)
+            if len(predator)>0:
+                print(R+"\nPredator Identity Details:\n")
+                for i in predator:
+                    Instagram(i)
+                    arr=os.listdir("./{}".format(str(i)))
+                    for j in arr:
+                        if re.match(r".+\.jpg",j):
+                            if imageai("./{}".format(str(i))+"/"+j) == True:
+                                print(R+"{} Is a Predator".format(str(i))+W)
+                print("Fetched Details are Saved at "+"./{0}/{1}.txt".format(username1,username1))
+                for i in predator:
+                    print("./{0}/{1}.txt".format(i,i))
+                    f=open("./{0}/{1}.txt".format(i,i),'r')
+                    message=f.read()
+                    f.close()
+
+                    #AutoMail Generated
+                    mail(message)
+            '''else:
+                print(R+"\nUser Profile Details:\n"+W)
+                print("Fetched Details are Saved at "+"./{0}/{1}.txt".format(username1,username1))
+                f=open("./{0}/{1}.txt".format(username1,username1),'r')
+                f.seek(0)
+                message=f.read()
+                f.close()
+                #AutoMail Generated
+                mail(message)'''
+
+    elif r.status_code == 404:
+        print(R+"Error: Profile Not Found")
+        exit()
+    else:
+        print(R+"Error: Something Went Wrong")
+        exit()
+    f1.close()
